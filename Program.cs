87de0c4cd9245a8b5851189ac2169b9c0b8404d2
Wei <willy830811@gmail.com>
2022-03-08 10:30:41 +0800

    project established

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using StackExchange.Redis;
+using System;
+
+namespace RedisApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var redis = ConnectionMultiplexer.Connect("localhost:6379");
+            var db = redis.GetDatabase();
+            db.StringSet("foo", 1688);
+        }
+    }
+}
